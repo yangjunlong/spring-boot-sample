5280138927735770239c2a6ab1f48142c22e8fa9
sobird <i@sobird.me>
2020-12-02 15:45:14 +0800

    添加spring.mvc.view.prefix/suffix配置

--- src/main/java/com/example/sample/SampleApplication.java
+++ src/main/java/com/example/sample/SampleApplication.java
@@ -2,16 +2,17 @@ package com.example.sample;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-@RestController
+@Controller
 @SpringBootApplication
 public class SampleApplication {
 
 	@RequestMapping("/")
 	String home() {
-		return "Hello World!";
+		return "index";
 	}
 	public static void main(String[] args) {
 		SpringApplication.run(SampleApplication.class, args);
